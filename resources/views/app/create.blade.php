238580f0e95c605ffc61679b3760e2863ecfe3a4
Fayi <fayi.pon@gmail.com>
2024-03-13 14:00:20 +0530

    Update create.blade.php

--- resources/views/app/create.blade.php
+++ resources/views/app/create.blade.php
@@ -11,28 +11,39 @@
 						
 						<div class="portlet">
 
-							<div class="portlet-header border-bottom">
-								<span>Default Bootstrap</span>
-							</div>
-
 							<div class="portlet-body">
 								<div class="container py-6">
 
 									<form>
-									<div class="form-group">
-										<label for="exampleInputEmail1">Email address</label>
-										<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-										<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-									</div>
-									<div class="form-group">
-										<label for="exampleInputPassword1">Password</label>
-										<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-									</div>
-									<div class="form-group form-check">
-										<input type="checkbox" class="form-check-input" id="exampleCheck1">
-										<label class="form-check-label" for="exampleCheck1">Check me out</label>
-									</div>
-									<button type="submit" class="btn btn-primary">Submit</button>
+										<div class="form-group">
+											<label>名称 * </label>
+											<input type="text" class="form-control" placeholder="请输入名称,2-30字符">
+										</div>
+										<div class="form-group">
+											<label>短名称 * </label>
+											<input type="text" class="form-control" placeholder="请输入名称,2-30字符">
+										</div>
+										<div class="form-group">
+											<label>安装页图标 * </label>
+											<input type="text" class="form-control" placeholder="请输入名称,2-30字符">
+											<small class="form-text text-muted">512x512尺寸, png格式,大小不超过1MB</small>
+										</div>
+										<div class="form-group">
+											<label>桌页图标 * </label>
+											<input type="text" class="form-control" placeholder="请输入名称,2-30字符">
+											<small class="form-text text-muted">192x192尺寸, png格式,大小不超过1MB</small>
+										</div>
+										<div class="form-group">
+											<label>游览器网页图标 * </label>
+											<input type="text" class="form-control" placeholder="请输入名称,2-30字符">
+											<small class="form-text text-muted">192x192尺寸, png格式,大小不超过1MB</small>
+										</div>
+										<div class="form-group">
+											<label>推广网址 * </label>
+											<input type="text" class="form-control" placeholder="请输入自已网站地址">
+										</div>
+
+										<button type="submit" class="btn btn-primary">提交</button>
 									</form>
 										
 
