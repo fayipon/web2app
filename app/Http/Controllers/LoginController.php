e49f6fca27bfa7b0bee56042ea65e9ac3f115307
Fayi <fayi.pon@gmail.com>
2024-03-13 16:44:58 +0530

    Update LoginController.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -28,8 +28,38 @@ class LoginController extends Controller
             $this->error(__CLASS__, __FUNCTION__, "01");
             return redirect('/login');
         }
+        
+        if ($return === null) {
+    		$this->error(__CLASS__, __FUNCTION__, "01");
+            return redirect('/login');
+        }
 
-        dd($return);
+
+        // 判断密码是否一致
+        if ($return['PASSWORD'] !== MD5($input['password'])) {
+            $this->error(__CLASS__, __FUNCTION__, "02");
+            return redirect('/login');
+        }
+
+        // 更新最后登入时间
+        $return = User::where("ACCOUNT",$input['account'])->update([
+            "UPDATE_TIME" => date("Y-m-d H:i:s")
+        ]);
+        if ($return === false) {
+            $this->error(__CLASS__, __FUNCTION__, "03");
+            return redirect('/login');
+        }
+
+        // 保存至session
+        unset($return['PASSWORD']);
+    	Session::put("user",$return);
+    	Session::put("is_login",1);
+        Session::Save();
+
+        // 操作纪录 TODO
+
+        $this->success(__CLASS__, __FUNCTION__, "01");
+        return redirect('/dashboard');
     }
 
     
