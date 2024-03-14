2c4fe55aed463e104988bca9988b7d80f93d9f17
Fayi <fayi.pon@gmail.com>
2024-03-14 14:14:06 +0530

    Update edit.blade.php

--- resources/views/app/edit.blade.php
+++ resources/views/app/edit.blade.php
@@ -32,44 +32,44 @@
 										</div>
 										<div class="form-group">
 											<label>短名称 * </label>
-											<input type="text" name="APP_SORT_NAME" class="form-control" placeholder="请输入名称,2-30字符">
+											<input type="text" name="APP_SORT_NAME" value="{{ $data['APP_SORT_NAME'] }}" class="form-control" placeholder="请输入名称,2-30字符">
 										</div>
 										<div class="form-group">
 											<label>安装页图标 * </label>
-											<input type="text" name="APP_SETUP_ICON" class="form-control" placeholder="请输入名称,2-30字符">
+											<input type="text" name="APP_SETUP_ICON" value="{{ $data['APP_SETUP_ICON'] }}" class="form-control" placeholder="请输入名称,2-30字符">
 											<small class="form-text text-muted">512x512尺寸, png格式,大小不超过1MB</small>
 										</div>
 										<div class="form-group">
 											<label>桌页图标 * </label>
-											<input type="text" name="APP_DESKTOP_ICON" class="form-control" placeholder="请输入名称,2-30字符">
+											<input type="text" name="APP_DESKTOP_ICON" value="{{ $data['APP_DESKTOP_ICON'] }}" class="form-control" placeholder="请输入名称,2-30字符">
 											<small class="form-text text-muted">192x192尺寸, png格式,大小不超过1MB</small>
 										</div>
 										<div class="form-group">
 											<label>游览器网页图标 * </label>
-											<input type="text" name="APP_BROWSER_ICON" class="form-control" placeholder="请输入名称,2-30字符">
+											<input type="text" name="APP_BROWSER_ICON" value="{{ $data['APP_BROWSER_ICON'] }}" class="form-control" placeholder="请输入名称,2-30字符">
 											<small class="form-text text-muted">192x192尺寸, png格式,大小不超过1MB</small>
 										</div>
 										<div class="form-group">
 											<label>推广网址 * </label>
-											<input type="text" name="APP_URL" class="form-control" placeholder="请输入自已网站地址">
+											<input type="text" name="APP_URL" value="{{ $data['APP_URL'] }}" class="form-control" placeholder="请输入自已网站地址">
 										</div>
 										<div class="form-group">
 											<label>网址是否允许修改</label>
 											<label class="form-switch form-switch-pill form-switch-primary d-block">
-												<input type="checkbox" name="IS_APP_URL_EDIT" value="1" checked="">
+												<input type="checkbox" name="IS_APP_URL_EDIT" value="{{ $data['IS_APP_URL_EDIT'] }}" value="1" checked="">
 												<i data-on="允许" data-off="不允许"></i>
 											</label>
 										</div>
 										<div class="form-group">
 											<label>在iframe打开</label>
 											<label class="form-switch form-switch-pill form-switch-primary d-block">
-												<input type="checkbox" name="IS_IFRAME" value="1" checked="">
+												<input type="checkbox" name="IS_IFRAME" value="{{ $data['IS_IFRAME'] }}" value="1" checked="">
 												<i data-on="是" data-off="否"></i>
 											</label>
 										</div>
 										<div class="form-group">
 											<label>应用安装地址 * </label>
-											<input type="text" name="SETUP_URL" class="form-control" placeholder="请输入自已网站地址">.xxxxx.com/download
+											<input type="text" name="SETUP_URL" value="{{ $data['SETUP_URL'] }}" class="form-control" placeholder="请输入自已网站地址">.xxxxx.com/download
 										</div>
 										<div class="form-group">
 											<label>iOS跳转地址 * </label>
@@ -77,20 +77,20 @@
 										</div>
 										<div class="form-group">
 											<label>开发者名称 * </label>
-											<input type="text" name="SETUP_DEV_NAME" class="form-control" placeholder="请输入开发者名称,2-30字符">
+											<input type="text" name="SETUP_DEV_NAME" value="{{ $data['SETUP_DEV_NAME'] }}" class="form-control" placeholder="请输入开发者名称,2-30字符">
 										</div>
 										<div class="form-group">
 											<label>评分 * </label>
-											<input type="text" name="SETUP_RATE" class="form-control" placeholder="请输入1.0-5.0评分">
+											<input type="text" name="SETUP_RATE" value="{{ $data['SETUP_RATE'] }}" class="form-control" placeholder="请输入1.0-5.0评分">
 										</div>
 										<div class="form-group">
 											<label>评价人数 * </label>
-											<input type="text" name="SETUP_RATE_P" class="form-control" placeholder="请输入1-10,000,000评分">
+											<input type="text" name="SETUP_RATE_P" value="{{ $data['SETUP_RATE_P'] }}" class="form-control" placeholder="请输入1-10,000,000评分">
 										</div>
 
 										<div class="form-group">
 											<label>安装人数 * </label>
-											<input type="text" name="SETUP_SETUP_P" class="form-control" placeholder="请输入1-10,000,000评分">
+											<input type="text" name="SETUP_SETUP_P" value="{{ $data['SETUP_SETUP_P'] }}" class="form-control" placeholder="请输入1-10,000,000评分">
 										</div>
 										<div class="form-group">
 											<label>适合年龄 * </label>
@@ -109,39 +109,39 @@
 										<div class="form-group">
 											<label>屏幕方向</label>
 											<label class="form-switch form-switch-pill form-switch-primary d-block">
-												<input type="checkbox" name="SCREEN_TYPE" value="1" checked="">
+												<input type="checkbox" name="SCREEN_TYPE" value="{{ $data['SCREEN_TYPE'] }}" checked="">
 												<i data-on="竖屏" data-off="横屏"></i>
 											</label>
 										</div>
 										
 										<div class="form-group">
 											<label>应用截图 * </label>
-											<input type="text" name="APP_SCREEN" class="form-control" placeholder="请输入名称,2-30字符">
+											<input type="text" name="APP_SCREEN" value="{{ $data['APP_SCREEN'] }}" class="form-control" placeholder="请输入名称,2-30字符">
 											<small class="form-text text-muted">330x587尺寸, png格式,大小不超过1MB</small>
 										</div>
 
 										<div class="form-group">
 											<label>应用介绍 * </label>
-											<textarea placeholder="Textarea" name="APP_DESCRIPTION" id="description" class="form-control" rows="3"></textarea>
+											<textarea placeholder="Textarea" name="APP_DESCRIPTION" value="{{ $data['APP_DESCRIPTION'] }}" id="description" class="form-control" rows="3"></textarea>
 										</div>
 
 										<div class="form-group">
 											<label>安装模版 * </label>
-											<input type="text" name="SETUP_TEMPLE" class="form-control" placeholder="请输入名称,2-30字符">
+											<input type="text" name="SETUP_TEMPLE" value="{{ $data['APP_NAME'] }}" class="form-control" placeholder="请输入名称,2-30字符">
 											<small class="form-text text-muted">330x587尺寸, png格式,大小不超过1MB</small>
 										</div>
 
 										<div class="form-group">
 											<label>点击任意位置安装</label>
 											<label class="form-switch form-switch-pill form-switch-primary d-block">
-												<input type="checkbox" name="IS_ANYWHERE_INSTALL" value="1" checked="">
+												<input type="checkbox" name="IS_ANYWHERE_INSTALL" value="{{ $data['IS_ANYWHERE_INSTALL'] }}" checked="">
 												<i data-on="是" data-off="否"></i>
 											</label>
 										</div>
 
 										<div class="form-group">
 											<label>备注 </label>
-											<input type="text" name="MARK" class="form-control" placeholder="备注,不会展示在安装页，仅用于管理app">
+											<input type="text" name="MARK" value="{{ $data['MARK'] }}" class="form-control" placeholder="备注,不会展示在安装页，仅用于管理app">
 										</div>
 
 										<div class="form-group">
@@ -154,7 +154,7 @@
 												</select>
 												<label for="select_options">第三方统计平台</label>
 											</div>
-											<input type="text" name="TRACKING_INFO" class="form-control" placeholder="请输入埋点id">
+											<input type="text" name="TRACKING_INFO" value="{{ $data['TRACKING_INFO'] }}" class="form-control" placeholder="请输入埋点id">
 										</div>
 
 										<button type="submit" class="btn btn-primary">提交</button>
