935cb2c49a1d8a002bfe5df7edab7c4e6cfbb18f
Fayi <fayi.pon@gmail.com>
2024-03-14 17:22:15 +0530

    dd

---
+++ app/Http/Controllers/ChannelController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+use App\Models\App;
+
+class ChannelController extends Controller
+{
+    // 首頁
+    public function index(Request $request) {
+    	
+        $this->isLogin();
+
+        $input = $this->getRequest($request);
+        $session = Session::all();
+        $this->assign("search",$input);
+
+        //////////////////////////////////////
+
+        // 取得当前用户的应用列表
+        $return = App::where("USER_ID",$session['user']['ID'])->get();
+        if ($return === false) {
+            $this->error(__CLASS__, __FUNCTION__, "01");
+            return redirect('/dashboard');
+        }
+        
+        $this->assign("list",$return);
+    	
+    	return view('channel.index',$this->data);
+    }
+}
+
