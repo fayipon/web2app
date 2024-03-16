97809d062ab2855490a3ef0c0736a4efd92cd604
Fayi <fayi.pon@gmail.com>
2024-03-16 16:47:45 +0530

    Update test.blade.php

--- resources/views/pv/test.blade.php
+++ resources/views/pv/test.blade.php
@@ -25,7 +25,8 @@
                     LANGUAGE: window.navigator.language,
                     SCREEN_WIDTH: window.screen.width,
                     SCREEN_HEIGHT: window.screen.height,
-                    URL: window.location.href
+                    URL: window.location.href,
+                    HOSTNAME: window.location.hostname
                 };
 
                 // 将 JavaScript 对象转换为 JSON 字符串
