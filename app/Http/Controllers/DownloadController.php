e38ef9b4235fc427856f36d6dbeff4fd2662158b
Fayi <fayi.pon@gmail.com>
2024-03-20 12:10:53 +0530

    Update DownloadController.php

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -15,7 +15,7 @@ class DownloadController extends Controller
         $this->assign("search",$input);
         
         // 配置
-        $this->assign("version",'demo002');
+        $this->assign("version",'demo003');
         
     	return view('download.index',$this->data);
     }
