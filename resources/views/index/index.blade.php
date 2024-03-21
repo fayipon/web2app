56daf25e6029da8730f85eea1c9266c4fec95d44
Fayi <fayi.pon@gmail.com>
2024-03-21 16:37:48 +0530

    index 页

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -28,4 +28,35 @@
             sendDataToAPI('SETUP_03');
         </script>
 
+    @if ($app_config['TRACKING_TYPE'] == 1)
+        <!-- Google Analytics 跟踪代码 -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id={{ $app_config['TRACKING_INFO'] }}"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '{{ $app_config["TRACKING_INFO"] }}');
+        </script>
+    @endif
+
+    @if ($app_config['TRACKING_TYPE'] == 2)
+        <!-- Facebook Pixel Code -->
+        <script>
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '{{ $app_config["TRACKING_INFO"] }}');
+            fbq('track', 'PageView');
+        </script>
+        <noscript><img height="1" width="1" style="display:none" 
+            src="https://www.facebook.com/tr?id={{ $app_config['TRACKING_INFO'] }}&ev=PageView&noscript=1"
+        /></noscript>
+        <!-- End Facebook Pixel Code -->
+    @endif
 </html>
