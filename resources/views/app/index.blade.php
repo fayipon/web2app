26dc2f51da9d7672ffe9d6ec0ea23d01dc28aaf1
Fayi <fayi.pon@gmail.com>
2024-03-22 10:32:10 +0530

    Update index.blade.php

--- resources/views/app/index.blade.php
+++ resources/views/app/index.blade.php
@@ -57,84 +57,129 @@
 											<div class="col-sm-12">
 												<table class="table-datatable table table-bordered table-hover table-striped js-datatableified dataTable dtr-inline" style="width: 1255px;">
 												<thead>
-													<tr>
-														<th style="width: 210px;">Name</th>
-														<th class="sorting" tabindex="0" aria-controls="rand_ijU" rowspan="1" colspan="1" data-column-index="1" style="width: 316px;" aria-label="Position: activate to sort column ascending">Position</th><th class="sorting" tabindex="0" aria-controls="rand_ijU" rowspan="1" colspan="1" data-column-index="2" style="width: 155px;" aria-label="Office: activate to sort column ascending">Office</th><th class="sorting" tabindex="0" aria-controls="rand_ijU" rowspan="1" colspan="1" data-column-index="3" style="width: 67px;" aria-label="Age: activate to sort column ascending">Age</th><th class="sorting" tabindex="0" aria-controls="rand_ijU" rowspan="1" colspan="1" data-column-index="4" style="width: 135px;" aria-label="Start date: activate to sort column ascending">Start date</th><th class="sorting" tabindex="0" aria-controls="rand_ijU" rowspan="1" colspan="1" data-column-index="5" style="width: 120px;" aria-label="Salary: activate to sort column ascending">Salary</th></tr>
-										</thead>
-										
-										<tbody>
-											
-										<tr class="odd">
-												<td class="dtr-control sorting_1" tabindex="0">Airi Satou</td>
-												<td>Accountant</td>
-												<td>Tokyo</td>
-												<td>33</td>
-												<td>2008/11/28</td>
-												<td>$162,700</td>
-											</tr><tr class="even">
-												<td class="sorting_1 dtr-control">Angelica Ramos</td>
-												<td>Chief Executive Officer (CEO)</td>
-												<td>London</td>
-												<td>47</td>
-												<td>2009/10/09</td>
-												<td>$1,200,000</td>
-											</tr><tr class="odd">
-												<td class="dtr-control sorting_1" tabindex="0">Ashton Cox</td>
-												<td>Junior Technical Author</td>
-												<td>San Francisco</td>
-												<td>66</td>
-												<td>2009/01/12</td>
-												<td>$86,000</td>
-											</tr><tr class="even">
-												<td class="sorting_1 dtr-control">Bradley Greer</td>
-												<td>Software Engineer</td>
-												<td>London</td>
-												<td>41</td>
-												<td>2012/10/13</td>
-												<td>$132,000</td>
-											</tr><tr class="odd">
-												<td class="sorting_1 dtr-control">Brenden Wagner</td>
-												<td>Software Engineer</td>
-												<td>San Francisco</td>
-												<td>28</td>
-												<td>2011/06/07</td>
-												<td>$206,850</td>
-											</tr><tr class="even">
-												<td class="dtr-control sorting_1" tabindex="0">Brielle Williamson</td>
-												<td>Integration Specialist</td>
-												<td>New York</td>
-												<td>61</td>
-												<td>2012/12/02</td>
-												<td>$372,000</td>
-											</tr><tr class="odd">
-												<td class="sorting_1 dtr-control">Bruno Nash</td>
-												<td>Software Engineer</td>
-												<td>London</td>
-												<td>38</td>
-												<td>2011/05/03</td>
-												<td>$163,500</td>
-											</tr><tr class="even">
-												<td class="sorting_1 dtr-control">Caesar Vance</td>
-												<td>Pre-Sales Support</td>
-												<td>New York</td>
-												<td>21</td>
-												<td>2011/12/12</td>
-												<td>$106,450</td>
-											</tr><tr class="odd">
-												<td class="sorting_1 dtr-control">Cara Stevens</td>
-												<td>Sales Assistant</td>
-												<td>New York</td>
-												<td>46</td>
-												<td>2011/12/06</td>
-												<td>$145,600</td>
-											</tr><tr class="even">
-												<td class="dtr-control sorting_1" tabindex="0">Cedric Kelly</td>
-												<td>Senior Javascript Developer</td>
-												<td>Edinburgh</td>
-												<td>22</td>
-												<td>2012/03/29</td>
-												<td>$433,060</td>
-											</tr></tbody>
+													<tr style="background-color:#e1e1e1;">
+														<th style="width: 210px;">应用ID</th>
+														<th style="width: 210px;">应用名称</th>
+														<th style="width: 210px;">状态</th>
+														<th style="width: 210px;">屏幕方向</th>
+														<th style="width: 210px;">安装量</th>
+														<th style="width: 210px;">创建时间</th>
+														<th style="width: 210px;">备注</th>
+														<th style="width: 210px;">链接</th>
+														<th style="width: 210px;">操作</th>
+												</thead>
+												<tbody>
+													<tr class="odd">
+														<td class="dtr-control sorting_1" tabindex="0">Airi Satou</td>
+														<td>Accountant</td>
+														<td>Tokyo</td>
+														<td>33</td>
+														<td>2008/11/28</td>
+														<td>$162,700</td>
+														<td>33</td>
+														<td>2008/11/28</td>
+														<td>$162,700</td>
+													</tr>
+													<tr class="odd">
+														<td class="dtr-control sorting_1" tabindex="0">Airi Satou</td>
+														<td>Accountant</td>
+														<td>Tokyo</td>
+														<td>33</td>
+														<td>2008/11/28</td>
+														<td>$162,700</td>
+														<td>33</td>
+														<td>2008/11/28</td>
+														<td>$162,700</td>
+													</tr>
+													<tr class="odd">
+														<td class="dtr-control sorting_1" tabindex="0">Airi Satou</td>
+														<td>Accountant</td>
+														<td>Tokyo</td>
+														<td>33</td>
+														<td>2008/11/28</td>
+														<td>$162,700</td>
+														<td>33</td>
+														<td>2008/11/28</td>
+														<td>$162,700</td>
+													</tr>
+													<tr class="odd">
+														<td class="dtr-control sorting_1" tabindex="0">Airi Satou</td>
+														<td>Accountant</td>
+														<td>Tokyo</td>
+														<td>33</td>
+														<td>2008/11/28</td>
+														<td>$162,700</td>
+														<td>33</td>
+														<td>2008/11/28</td>
+														<td>$162,700</td>
+													</tr>
+													<tr class="odd">
+														<td class="dtr-control sorting_1" tabindex="0">Airi Satou</td>
+														<td>Accountant</td>
+														<td>Tokyo</td>
+														<td>33</td>
+														<td>2008/11/28</td>
+														<td>$162,700</td>
+														<td>33</td>
+														<td>2008/11/28</td>
+														<td>$162,700</td>
+													</tr>
+													<tr class="odd">
+														<td class="dtr-control sorting_1" tabindex="0">Airi Satou</td>
+														<td>Accountant</td>
+														<td>Tokyo</td>
+														<td>33</td>
+														<td>2008/11/28</td>
+														<td>$162,700</td>
+														<td>33</td>
+														<td>2008/11/28</td>
+														<td>$162,700</td>
+													</tr>
+													<tr class="odd">
+														<td class="dtr-control sorting_1" tabindex="0">Airi Satou</td>
+														<td>Accountant</td>
+														<td>Tokyo</td>
+														<td>33</td>
+														<td>2008/11/28</td>
+														<td>$162,700</td>
+														<td>33</td>
+														<td>2008/11/28</td>
+														<td>$162,700</td>
+													</tr>
+													<tr class="odd">
+														<td class="dtr-control sorting_1" tabindex="0">Airi Satou</td>
+														<td>Accountant</td>
+														<td>Tokyo</td>
+														<td>33</td>
+														<td>2008/11/28</td>
+														<td>$162,700</td>
+														<td>33</td>
+														<td>2008/11/28</td>
+														<td>$162,700</td>
+													</tr>
+													<tr class="odd">
+														<td class="dtr-control sorting_1" tabindex="0">Airi Satou</td>
+														<td>Accountant</td>
+														<td>Tokyo</td>
+														<td>33</td>
+														<td>2008/11/28</td>
+														<td>$162,700</td>
+														<td>33</td>
+														<td>2008/11/28</td>
+														<td>$162,700</td>
+													</tr>
+													<tr class="odd">
+														<td class="dtr-control sorting_1" tabindex="0">Airi Satou</td>
+														<td>Accountant</td>
+														<td>Tokyo</td>
+														<td>33</td>
+														<td>2008/11/28</td>
+														<td>$162,700</td>
+														<td>33</td>
+														<td>2008/11/28</td>
+														<td>$162,700</td>
+													</tr>
+											</tbody>
 										<tfoot>
 											<tr><th rowspan="1" colspan="1">Name</th><th rowspan="1" colspan="1">Position</th><th rowspan="1" colspan="1">Office</th><th rowspan="1" colspan="1">Age</th><th rowspan="1" colspan="1">Start date</th><th rowspan="1" colspan="1">Salary</th></tr>
 										</tfoot>
