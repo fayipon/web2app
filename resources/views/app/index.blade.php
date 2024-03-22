da8da657daa906b22ea52de0530f5edf6bb975a9
Fayi <fayi.pon@gmail.com>
2024-03-22 10:25:02 +0530

    Update index.blade.php

--- resources/views/app/index.blade.php
+++ resources/views/app/index.blade.php
@@ -45,7 +45,7 @@
 											<div class="col-sm-12 col-md-6 d-flex align-items-center justify-content-end">
 												<div class="dt-buttons btn-group flex-wrap"> 
 													<div class="btn-group">
-														<a href="/app-create" class="btn btn-secondary buttons-collection dropdown-toggle buttons-colvis btn-sm btn-light" tabindex="0" aria-controls="rand_ijU" type="button" title="Column Visibility" aria-haspopup="true">
+														<a href="/app-create" class="btn btn-secondary buttons-collection dropdown-toggle buttons-colvis btn-sm btn-info" tabindex="0" aria-controls="rand_ijU" type="button" title="Column Visibility" aria-haspopup="true">
 															<span>创建应用</span>
 															<span class="dt-down-arrow"></span>
 														</a>
@@ -55,7 +55,7 @@
 										</div>
 										<div class="row">
 											<div class="col-sm-12">
-												<table class="table-datatable table table-bordered table-hover table-striped js-datatableified dataTable dtr-inline" data-lng-empty="No data available in table" data-lng-page-info="Showing _START_ to _END_ of _TOTAL_ entries" data-lng-filtered="(filtered from _MAX_ total entries)" data-lng-loading="Loading..." data-lng-processing="Processing..." data-lng-search="Search..." data-lng-norecords="No matching records found" data-lng-sort-ascending=": activate to sort column ascending" data-lng-sort-descending=": activate to sort column descending" data-main-search="true" data-column-search="false" data-row-reorder="false" data-col-reorder="true" data-responsive="true" data-header-fixed="true" data-select-onclick="true" data-enable-paging="true" data-enable-col-sorting="true" data-autofill="false" data-group="false" data-items-per-page="10" data-enable-column-visibility="true" data-lng-column-visibility="Column Visibility" data-enable-export="true" data-lng-export="<i class='fi fi-squared-dots fs--18 line-height-1'></i>" data-lng-csv="CSV" data-lng-pdf="PDF" data-lng-xls="XLS" data-lng-copy="Copy" data-lng-print="Print" data-lng-all="All" data-export-pdf-disable-mobile="true" data-export="[&quot;csv&quot;, &quot;pdf&quot;, &quot;xls&quot;]" data-options="[&quot;copy&quot;, &quot;print&quot;]" data-custom-config="{}" id="rand_ijU" aria-describedby="rand_ijU_info" style="width: 1255px;">
+												<table class="table-datatable table table-bordered table-hover table-striped js-datatableified dataTable dtr-inline" style="width: 1255px;">
 												<thead>
 													<tr>
 														<th class="sorting sorting_asc" tabindex="0" aria-controls="rand_ijU" rowspan="1" colspan="1" data-column-index="0" style="width: 210px;" aria-sort="ascending" aria-label="Name: activate to sort column descending">Name</th>
