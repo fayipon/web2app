250f6a8d4212c59e6e19a09208f4f24986d3c023
Fayi <fayi.pon@gmail.com>
2024-03-23 10:52:07 +0530

    Update SubscriptionController.php

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -18,6 +18,12 @@ class SubscriptionController extends Controller
         ]);
 
         // 保存订阅信息到数据库或其他存储方式
+        $subscription = [
+            'endpoint' => $request->input('endpoint'),
+            'p256dh' => $request->input('keys.p256dh'),
+            'auth' => $request->input('keys.auth'),
+        ];
+        dd($subscription);
 
         // 进行其他逻辑处理，例如发送欢迎邮件、记录日志等
 
