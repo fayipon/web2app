6108137580d8160d461ed6401e3bf801c3eb72f6
Fayi <fayi.pon@gmail.com>
2024-03-23 12:22:32 +0530

    Update test.blade.php

--- resources/views/index/test.blade.php
+++ resources/views/index/test.blade.php
@@ -16,7 +16,7 @@
                     if (permission === 'granted') {
 
                         // 转换 VAPID 公钥为 Uint8Array 格式
-                        const vapidPublicKey = `MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEoyLBNLyYxXVEHlbWCMFsCi4jZWiCAF1JVFNbqchjAZtDv8MFTt96+wbwaDylt8p1smQaI8fwwuz7mOshTJE/tA==`;
+                        const vapidPublicKey = 'MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEoyLBNLyYxXVEHlbWCMFsCi4jZWiCAF1JVFNbqchjAZtDv8MFTt96+wbwaDylt8p1smQaI8fwwuz7mOshTJE/tA==';
 
                         const applicationServerKey = urlBase64ToUint8Array(vapidPublicKey);
                         
