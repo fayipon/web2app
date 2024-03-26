563118b65a6c303e02e8ef1f2bf5579b87d5a03d
Fayi <fayi.pon@gmail.com>
2024-03-26 19:31:00 +0530

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -15,20 +15,20 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['web-api/*'],
 
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'],
-
+    'allowed_methods' => ['GET', 'POST'],
+    
+    'allowed_origins' => ['https://chjdhbyk.top/'],
+    
     'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['*'],
-
+    
+    'allowed_headers' => ['Content-Type', 'X-Requested-With'],
+    
     'exposed_headers' => [],
-
+    
     'max_age' => 0,
-
+    
     'supports_credentials' => false,
 
 ];
