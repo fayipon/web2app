6597edc0900e9f69899eb180e343c876404c4b7b
Fayi <fayi.pon@gmail.com>
2024-03-26 19:23:12 +0530

    dd

--- routes/web.php
+++ routes/web.php
@@ -61,3 +61,4 @@ Route::get('/subscribe', 'App\Http\Controllers\SubscriptionController@subscribe'
 // PG 测试用
 
 Route::get('/web-api/auth/session/v2/verifySession', 'App\Http\Controllers\GamePGController@verifySession');
+Route::post('/web-api/auth/session/v2/verifySession', 'App\Http\Controllers\GamePGController@verifySession');
