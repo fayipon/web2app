6464310e75570f9f065da3d0811518f081dc7b51
Fayi <fayi.pon@gmail.com>
2024-03-27 10:59:03 +0530

    dd

--- public/shared/3c4695a542/shell-res/ad/ad_adv_dfp_test.js
+++ public/shared/3c4695a542/shell-res/ad/ad_adv_dfp_test.js
@@ -1,1 +1,7 @@
-Your file content here
+var e = document.createElement('div');
+e.id = 'MWUOvoYpNAmf';
+e.style.display = 'none';
+document.body.appendChild(e);
+setTimeout(() => {
+    document.body.removeChild(e);
+}, 2000);
